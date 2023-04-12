f608ff50138989ef790784e566ae39539480fd9b
Majid Aghighi <majid.aghighy@gmail.com>
2023-04-12 19:44:09 +0330

    Solve Question 3

---
+++ Question3/script.js
@@ -0,0 +1,34 @@
+const values = [
+  "Hare",
+  "Krishna",
+  "Hare",
+  "Krishna",
+  "Krishna",
+  "Krishna",
+  "Hare",
+  "Hare",
+  ":-O",
+];
+
+// Number 1
+// function unique(arr) {
+//   return [...new Set(arr)];
+// }
+
+// console.log(...unique(values));
+
+//--------------------------------------------------------
+// Number 2
+// function unique(arr) {
+//   const uniqueChars = [];
+//   arr.forEach((element) => {
+//     if (!uniqueChars.includes(element)) {
+//       uniqueChars.push(element);
+//     }
+//   });
+
+//   return uniqueChars;
+
+// }
+
+// console.log(...unique(values));
